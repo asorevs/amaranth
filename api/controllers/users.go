a5c4fa3b93da5a85e93ed448938672978eb1c6a7
Alberto Soto <asotorev@outlook.com>
2023-04-27 09:50:46 -0600

    add basic error handling

--- api/controllers/users.go
+++ api/controllers/users.go
@@ -3,8 +3,8 @@ package controllers
 import (
 	"amaranth/api/models"
 	"amaranth/api/services"
+	"amaranth/api/utils"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 )
@@ -23,26 +23,26 @@ type usersController struct{}
 func (c *usersController) Create(w http.ResponseWriter, r *http.Request) {
 
 	var user models.User
-	fmt.Println("\n", "user", user)
 	bytes, err := io.ReadAll(r.Body)
 	if err != nil {
-		//TODO: Handle error
+		err := utils.NewBadRequestError("invalid json body")
+		utils.RespondError(w, *err)
 		return
 	}
 	if err := json.Unmarshal(bytes, &user); err != nil {
-		//TODO: Handle error
+		restErr := utils.NewBadRequestError("invalid json body")
+		utils.RespondError(w, *restErr)
 		return
 	}
 
 	result, saveErr := services.UsersService.CreateUser(user)
 	if saveErr != nil {
-		//TODO: Handle error
+		saveErr := utils.NewBadRequestError("error saving data")
+		utils.RespondError(w, *saveErr)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(result)
+	utils.RespondJson(w, http.StatusCreated, result)
 
 }
 
